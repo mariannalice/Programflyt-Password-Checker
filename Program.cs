2871150b95907cefb8edeb74e45c3b3b02610174
wam0824 <wam082024@gmail.com>
2025-05-15 10:38:59 +0200

    Enkel programflyt

--- Program.cs
+++ Program.cs
@@ -2,6 +2,24 @@
 {
   static void Main ()
   {
-    
+    Console.WriteLine("Please enter you password. ");
+    string? Password = Console.ReadLine();
+
+   if (Password.Length <=6)
+   {
+      Console.WriteLine($"Your password is: {Password.Length} characters. 6 og less characters is a weak password.");
+   }
+   else if (Password.Length >= 6 && Password.Length <= 10)
+   {
+    Console.WriteLine($"Your pawwsord is: {Password.Length} characters. Between 6 and 10 characters is a medium-strong password");
+   }
+   else if (Password.Length >= 10)
+   {
+    Console.WriteLine($"10 characters or more is a strong password. You password contains: {Password.Length} characters.");
+   }
+   else
+   {
+    Console.WriteLine("");
+   }
   }
 }
