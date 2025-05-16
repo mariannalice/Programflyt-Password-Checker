0a395f071f7dc3a1c0ea4fb2275031786e0d7d13
wam0824 <wam082024@gmail.com>
2025-05-16 12:46:17 +0200

    Endringer i oppsett

--- Program.cs
+++ Program.cs
@@ -2,24 +2,35 @@
 {
   static void Main ()
   {
-    Console.WriteLine("Please enter you password. ");
-    string? Password = Console.ReadLine();
+    string? Password = null;
+    while (string.IsNullOrWhiteSpace(Password))
+    {  
+      Console.WriteLine("Please enter your password:");
+      Password = Console.ReadLine();
 
-   if (Password.Length <=6)
-   {
-      Console.WriteLine($"Your password is: {Password.Length} characters. 6 og less characters is a weak password.");
-   }
-   else if (Password.Length >= 6 && Password.Length <= 10)
-   {
-    Console.WriteLine($"Your pawwsord is: {Password.Length} characters. Between 6 and 10 characters is a medium-strong password");
-   }
-   else if (Password.Length >= 10)
-   {
-    Console.WriteLine($"10 characters or more is a strong password. You password contains: {Password.Length} characters.");
-   }
-   else
-   {
-    Console.WriteLine("");
-   }
+      if (string.IsNullOrWhiteSpace(Password))
+      {
+        Console.WriteLine("You need to enter a valid password. Not empty or whith space between characters.\n");
+      }
+    }
+
+    Console.WriteLine("\nLets check the strength of your password!");
+      
+    if (Password.Length >=1 && Password.Length <=6)
+    {
+        Console.WriteLine($"Your password is: {Password.Length} characters. 6 og less characters is a weak password.");
+    }
+    else if (Password.Length >= 6 && Password.Length <= 10)
+    {
+      Console.WriteLine($"Your password is: {Password.Length} characters. Between 6 and 10 characters is a medium-strong password");
+    }
+    else if (Password.Length > 10)
+    {
+      Console.WriteLine($"10 characters or more is a strong password. You password contains: {Password.Length} characters.");
+    }
+    else
+    {
+      Console.WriteLine("");
+    }
   }
 }
